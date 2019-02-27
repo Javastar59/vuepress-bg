0761b63d84cdb993684947ffcf59ecb22e19acc3
star <sunstar59@163.com>
2019-02-27 10:03:25 +0800

    add docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Hello-VuePress',
+    description: '',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' }
+        ],
+        sidebar: 'auto'
+    }
+}
