75064efce418d78a594fc163189fa8df5b5c779f
star <sunstar59@163.com>
2019-02-27 17:36:11 +0800

    add

--- docs/.vuepress/theme/PageRight.vue
+++ docs/.vuepress/theme/PageRight.vue
@@ -6,10 +6,10 @@
         <img class="user-img" src="/user-img.png" />
       </div>
       <div>
-        <div class="info">姓名：王东杰</div>
-        <div class="info">昵称：隔壁老王</div>
-        <div class="info">爱好：撸猫</div>
-        <div class="info">简介：一枚喜欢撸猫的web前端开发</div>
+        <div class="info">姓名：曹兴</div>
+        <div class="info">昵称：star</div>
+        <div class="info">爱好：电影</div>
+        <div class="info">简介：一枚Java开发程序猿</div>
       </div>
       <a class="more-about" href="/about/">更多</a>
     </div>
@@ -31,7 +31,7 @@
   @import './styles/config.styl'
   .d-page-right{
     width 26%
-    float left 
+    float left
     margin-left 2%
     border-radius 2px
     text-align left
@@ -58,7 +58,7 @@
         text-align center
         height 30px
         line-height 30px
-        border-top 1px solid #eeeeee 
+        border-top 1px solid #eeeeee
         width 100%
         font-size 12px
         display inline-block
