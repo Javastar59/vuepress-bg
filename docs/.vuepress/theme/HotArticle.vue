8562608e1e2c0e94f1c1f31ea4ba645b84931423
star <sunstar59@163.com>
2019-03-09 18:22:38 +0800

    update

--- docs/.vuepress/theme/HotArticle.vue
+++ docs/.vuepress/theme/HotArticle.vue
@@ -37,7 +37,6 @@
     methods: {
       handleScroll () {
         const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-        console.log(scrollTop)
         if (scrollTop > 290) {
           this.barFixed = true;
         }else {
